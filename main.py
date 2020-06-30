45d627e91fb1e37cac1056fe880bd9d0bfb6fd38
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-06-30 21:28:57 +0530

    retry

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import math
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 
-server = 'local'
+server = 'prod'
 app=Flask(__name__)
 
 app.secret_key = 'super-secret-key'
