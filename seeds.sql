90b7c9040ef97a8af388c4cf2e50f8854b035ccc
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-06-30 12:23:44 +0530

    adding sql files

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts(
+	sno, title, tagline, slug, content, img_file, credit, post_img, date)
+	VALUES (1, 'first title', 'first tagline', 'first-post', 'content of a first post', 'home-bg.jpg', 'admin', 'home-ng.jpg', '30-6-2020');
