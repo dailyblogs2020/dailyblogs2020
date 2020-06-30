ddeb3345824fc714aadf311cd3e80d3a0b1e9c8c
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-06-30 18:54:40 +0530

    posts solved

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts(
+	sno, title, tagline, slug, content, img_file, credit, post_img, date)
+	VALUES (1, 'first title', 'first tagline', 'first-post', 'content of first post', 'home-bg.jpg', 'admin', 'home-bg.jpg', '2020-06-30');
