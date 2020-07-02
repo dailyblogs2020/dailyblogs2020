4e9bc4ed3b2e9adba8756fcc24a52c5834504602
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-07-02 22:41:23 +0530

    make it better

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO posts(
-	sno, title, tagline, slug, href_link, content, img_file, credit, post_img, date)
-	VALUES (1, 'first title', 'first tagline', 'first-post',' ' , 'content of first post', 'home-bg.jpg', 'admin', 'home-bg.jpg', '2020-06-30');
+	sno, title, tagline, slug, href_link, content, img_file, credit, conclusion, post_img, date)
+	VALUES (1, 'first title', 'first tagline', 'first-post','https://www.youtube.com/watch?v=Q8rN3JKqUc8' , 'content of first post', 'home-bg.jpg', 'admin', 'conclusion of a first post', 'home-bg.jpg', '2020-06-30');
