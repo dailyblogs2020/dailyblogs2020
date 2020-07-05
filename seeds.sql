e694ca60a6bc335482d5fb59cace743d07c04ef1
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-07-05 22:45:19 +0530

    added post link

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO posts(
-	sno, title, tagline, slug, href_link, content, img_file, credit, conclusion, post_img, date)
-	VALUES (1, 'first title', 'first tagline', 'first-post','https://www.youtube.com/watch?v=Q8rN3JKqUc8' , 'content of first post', 'home-bg.jpg', 'admin', 'conclusion of a first post', 'home-bg.jpg', '2020-06-30');
+	sno, title, tagline, slug, href_link, post_link, content, img_file, credit, conclusion, post_img, date)
+	VALUES (1, 'first title', 'first tagline', 'first-post','https://www.youtube.com/watch?v=Q8rN3JKqUc8', 'https://www.youtube.com/watch?v=Q8rN3JKqUc8' , 'content of first post', 'home-bg.jpg', 'admin', 'conclusion of a first post', 'home-bg.jpg', '2020-06-30');
