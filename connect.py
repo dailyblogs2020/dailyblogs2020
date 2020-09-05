934c299db1d469f0a2ff4a75400194b36a1dd5f7
dailyblogs2021 <dailyblogs,team@gmail.com>
2020-09-05 18:55:50 +0530

    make it better

--- connect.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector  
-     
-myconn = mysql.connector.connect(host = "ec2-52-20-248-222.compute-1.amazonaws.com", user = "yizavtynwgjfcs",passwd = "aff6e5ec08f5035f2152457497cd4da56d6cecce2d7e0f83b94f1121b8af3f8b", database = "dcot1pjik8e8oj")
-    
-print(myconn)
